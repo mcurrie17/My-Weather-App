cc14628327d2183cebf29f969c8c483e4d23c931
Megan <107447664+mcurrie17@users.noreply.github.com>
2022-07-14 10:00:57 -0500

    Added JS for weather conditions

--- src/app.js
+++ src/app.js
@@ -1,2 +1,19 @@
 let apiKey = "095d2e824ddba0bb0037c48b7b065155";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+let city = "Austin";
+let units = "imperial";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+function displayTemp(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#windSpeed");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windSpeedElement.innerHTML = response.data.wind.speed;
+}
+
+axios.get(apiUrl).then(displayTemp);
